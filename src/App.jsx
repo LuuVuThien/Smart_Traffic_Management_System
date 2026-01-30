044a1b15d585a759c7442785aab8a99062a036ff
Siba-2005T <bass030196@gmail.com>
2026-01-30 13:53:36 +0700

    Khoi tao du an STMS Frontend va cau truc thu muc

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>🚦 STMS Frontend Project</h1>
+    </div>
+  );
+}
+
+export default App;
